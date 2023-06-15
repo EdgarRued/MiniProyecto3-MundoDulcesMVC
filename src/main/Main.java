aaa43dd9bb2843aaeb5e4b99c2ba5028782a8ca6
Adrianfva <adrian.velasquez@correounivalle.edu.co>
2023-06-15 18:07:46 -0500

    Interfaz grafica en implementacion, 1ras dos funciones OK

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
+import controlador.Controlador;
+import logica.Dulce;
+import vistas.VistaDulceria;
+import vistas.VistaGUI;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("hello wo");
+        Dulce dulce = new Dulce();
+        VistaDulceria vista = new VistaGUI();
+        Controlador ctrl = new Controlador(vista, dulce);
+        ctrl.iniciar();
     }
 }
