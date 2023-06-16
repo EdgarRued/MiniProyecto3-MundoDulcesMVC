685187d8eba7a736d474d2bac21d134065ffb455
EdgarRued <edgar.rueda@correounivalle.edu.co>
2023-06-15 21:12:29 -0500

    Ultimas 2 funcionalidades de la interfaz grafica TERMINADAS

--- src/vistas/Menu2.java
+++ src/vistas/Menu2.java
@@ -22,7 +22,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.util.ArrayList;
+
 
 
 public class Menu2 extends JFrame implements ActionListener,ItemListener,VistaDulceria{
